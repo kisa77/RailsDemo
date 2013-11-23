42866423210ff8de51bc92e7bb7633ed6cae20f3
Nemok <kisa77.lee@gmail.com>
2013-11-23 17:06:49 +0800

    add line_item

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Demo::Application.routes.draw do
+  resources :line_items
+
     resources :carts
     resources :people
     resources :event
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131123085030) do
+ActiveRecord::Schema.define(version: 20131123090628) do
 
   create_table "carts", force: true do |t|
     t.datetime "created_at"
@@ -23,6 +23,13 @@ ActiveRecord::Schema.define(version: 20131123085030) do
     t.text     "description"
     t.boolean  "is_public"
     t.integer  "capacity"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "line_items", force: true do |t|
+    t.integer  "product_id"
+    t.integer  "cart_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
