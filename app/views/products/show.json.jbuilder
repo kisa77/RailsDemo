8147accdb1bd6b4a097be7acd271445089fea668
Nemok <kisa77.lee@gmail.com>
2013-11-26 09:14:11 +0800

    提交product

---
+++ app/views/products/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @product, :titile, :description, :image_url, :price, :created_at, :updated_at
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131123090628) do
+ActiveRecord::Schema.define(version: 20131126011032) do
 
   create_table "carts", force: true do |t|
     t.datetime "created_at"
@@ -41,6 +41,15 @@ ActiveRecord::Schema.define(version: 20131123090628) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "status"
+  end
+
+  create_table "products", force: true do |t|
+    t.string   "titile"
+    t.text     "description"
+    t.string   "image_url"
+    t.decimal  "price",       precision: 8, scale: 2
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
