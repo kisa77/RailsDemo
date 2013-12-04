c370c03ca69b10723d3d1094acb4f8b49102c225
Nemok <kisa77.lee@gmail.com>
2013-12-04 16:29:08 +0800

    add file to test railsadmin

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131204055634) do
+ActiveRecord::Schema.define(version: 20131204081101) do
 
   create_table "carts", force: true do |t|
     t.datetime "created_at"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20131204055634) do
     t.integer  "capacity"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "title"
   end
 
   create_table "line_items", force: true do |t|
